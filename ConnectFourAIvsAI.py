3b120d5dfb6a3ea0aa80d8cb8dbcf0da2ea2c558
Amelia <13049856+AmeliaKh@users.noreply.github.com>
2017-09-29 20:03:56 +0100

    Update ConnectFourAIvsAI.py

--- ConnectFourAIvsAI.py
+++ ConnectFourAIvsAI.py
@@ -1,4 +1,4 @@
-import ConnectFourEngine
+mport ConnectFourEngine
 import C4_AlphaBeta
 import C4_Deep
 import C4_Minimax
@@ -6,9 +6,12 @@ import Engine_minus_graphics
 
 if __name__ == '__main__':
 
+# the Deep AI and Alpha Beta AI should only be white players and the Minimax AI should only be a brown player
+
     app = ConnectFourEngine.ConnectFour(
             set_rewards=[0.000002, 0.0001, 0, 0, 0, 0, 0, 0, 0], # needed when playing with Deep
             set_white_player = C4_Deep.AIcheck,
             set_brown_player = C4_Minimax.AIcheck,
             )
     app.game_loop()
+
