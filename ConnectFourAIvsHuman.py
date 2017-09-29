6cfbdbcf8dc1c1933e8b261c479be3acf16f71c5
Amelia <13049856+AmeliaKh@users.noreply.github.com>
2017-09-29 20:03:32 +0100

    Update ConnectFourAIvsHuman.py

--- ConnectFourAIvsHuman.py
+++ ConnectFourAIvsHuman.py
@@ -1,16 +1,16 @@
-import ConnectFourMinimaxAI
-import HeuristicsBROWN_deep
-import ConnectFourEngine2
-
+import ConnectFourEngine
+import C4_AlphaBeta
+import C4_Deep
+import C4_Minimax
+import Engine_minus_graphics
 
 if __name__ == '__main__':
-    # Initialise the game engine
-    # Modify these parameters to tweak the game
-    app = ConnectFourEngine2.ConnectFour(
-        #red = white     blue = brown
-            set_rewards=[0.000002, 0.0001, 0, 0, 0, 0, 0, 0, 0], # needed when playing with white2
-            set_white_player = None,
+
+# the Deep AI and Alpha Beta AI should only be white players and the Minimax AI should only be a brown player
+
+    app = ConnectFourEngine.ConnectFour(
+            set_rewards=[0.000002, 0.0001, 0, 0, 0, 0, 0, 0, 0], # needed when playing with Deep
+            set_white_player = C4_Deep.AIcheck,
             set_brown_player = None,
             )
-    # start the game engine
     app.game_loop()
